528f9a58b976495f60947fbc1e6c3d68b877a15b
Uli-Z <58443149+Uli-Z@users.noreply.github.com>
2025-11-16 19:01:24 +0100

    feat(trpc): add import endpoints (preview, start, run-chunk, cancel, finalize)
    - Expose preview endpoint to parse and summarize uploaded file before import
    - Implement job-based create flow with chunked processing and progress reporting
    - Provide cancel/cleanup and finalize endpoints to control lifecycle
    - Register endpoints under groups router
    
    (cherry picked from commit d5f4ae8aaf1a946e1880ee0ebdf9771a8cb0ec79)

--- src/trpc/routers/groups/index.ts
+++ src/trpc/routers/groups/index.ts
@@ -7,6 +7,14 @@ import { getGroupProcedure } from '@/trpc/routers/groups/get.procedure'
 import { groupStatsRouter } from '@/trpc/routers/groups/stats'
 import { updateGroupProcedure } from '@/trpc/routers/groups/update.procedure'
 import { getGroupDetailsProcedure } from './getDetails.procedure'
+import {
+  cancelCreateImportFromFileProcedure,
+  finalizeCreateImportFromFileProcedure,
+  importGroupFromFileProcedure,
+  previewImportGroupFromFileProcedure,
+  runCreateImportFromFileChunkProcedure,
+  startCreateImportFromFileProcedure,
+} from './import'
 import { listGroupsProcedure } from './list.procedure'
 import { listAllGroupsProcedure } from './listAll.procedure'
 
@@ -21,5 +29,11 @@ export const groupsRouter = createTRPCRouter({
   list: listGroupsProcedure,
   listAll: listAllGroupsProcedure,
   create: createGroupProcedure,
+  importFromFile: importGroupFromFileProcedure,
+  importFromFilePreview: previewImportGroupFromFileProcedure,
+  importFromFileStartJob: startCreateImportFromFileProcedure,
+  importFromFileRunChunk: runCreateImportFromFileChunkProcedure,
+  importFromFileCancelJob: cancelCreateImportFromFileProcedure,
+  importFromFileFinalize: finalizeCreateImportFromFileProcedure,
   update: updateGroupProcedure,
 })
